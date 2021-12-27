b8d387903a4e5047e704153982cdb74bc2dcc1b3
medkan01 <mehdiakniou@gmail.com>
2021-12-27 14:06:15 +0100

    ajout des méthodes translation, homothetie et rotation à Forme.h

--- Forme.h
+++ Forme.h
@@ -2,9 +2,33 @@
 #include <iostream>
 #include <string>
 
+#include "Vecteur2D.h"
+
 using namespace std;
 
 class Forme {
-	 string couleur;
+	string couleur;
+public:
+	/**
+	* Effectue une translation sur la forme.
+	* 
+	* @param const Vecteur2D& v - Vecteur de translation
+	*/
+	virtual void translation(const Vecteur2D& v) const = 0;
 
+	/**
+	* Effectue une homothétie sur la forme. (zoom)
+	* 
+	* @param const Vecteur2d& - Point invariant.
+	* @param const double& - Rapport d'homothétie.
+	*/
+	virtual void homothetie(const Vecteur2D& pi, const double& rh) const = 0;
+
+	/**
+	* Effectue une rotation sur la forme.
+	* 
+	* @param const Vecteur2D& pi - Point invariant. (centre de rotation)
+	* @param const double& angle - Angle de rotation. (en radian)
+	*/
+	virtual void rotation(const Vecteur2D& pi, const double& angle) const = 0;
 };
