9e3ba754b06a21bbcde1dd051398a15abc02d193
medkan01 <mehdiakniou@gmail.com>
2022-01-05 21:42:20 +0100

    modification de la classe pour remplacer les coordonnées x et y en double plutôt qu'en int + modif docstring

--- Vecteur2D.h
+++ Vecteur2D.h
@@ -10,15 +10,15 @@ using namespace std;
 class Vecteur2D {
 public:
 	// Attributs
-	int x, y;
+	double x, y;
 
 	/**
 	* Constructeur de la classe Vecteur2D qui contruit un vecteur u(u1, u2).
 	*
-	* @param x est la valeur de u1 dans le vecteur u(u1, u2).
-	* @param y est la valeur de u2 dans le vecteur u(u1, u2).
+	* @param const double& x - valeur de u1 dans le vecteur u(u1, u2).
+	* @param const double& y - valeur de u2 dans le vecteur u(u1, u2).
 	*/
-	explicit Vecteur2D(int x = 0, int y = 0) : x(x), y(y) {}
+	explicit Vecteur2D(const double& x = 0, const double& y = 0) : x(x), y(y) {}
 
 	///	Destructeur de la classe Vecteur2D.
 	virtual ~Vecteur2D() {}
@@ -26,22 +26,22 @@ public:
 	/**
 	* Addtition de deux vecteurs u et v.
 	*
-	* @param v le vecteur qui sera additionné avec le vecteur u courant.
-	* @return le vecteur résultant de l'addition des vecteurs u et v.
+	* @param const Vecteur2D& v - vecteur qui sera additionné avec le vecteur u courant.
+	* @return const Vecteur2D - vecteur résultant de l'addition des vecteurs u et v.
 	*/
 	const Vecteur2D operator+(const Vecteur2D& v) const;
 
 	/**
 	* Opérateur - unaire. Permet entre autre d'opposer un vecteur.
 	*
-	* @return l'opposé du vecteur courant.
+	* @return const Vecteur2D - opposé du vecteur courant.
 	*/
 	const Vecteur2D operator-() const;
 
 	/**
 	* Soustraction de deux vecteurs u et v.
 	*
-	* @param v le vecteur qui sera soustrait avec le vecteur u courant.
+	* @param const Vecteur2D& v - vecteur qui sera soustrait avec le vecteur u courant.
 	* @return le vecteur résultant de la soustraction des vecteurs u et v.
 	*/
 	const Vecteur2D operator-(const Vecteur2D& v) const;
@@ -57,10 +57,10 @@ public:
 	/**
 	* Multiplication d'un vecteur par un entier i.
 	*
-	* @param i l'entier qui sera multiplié avec le vecteur u courant.
-	* @return le vecteur résultant de la multiplication de l'entier i et du vecteur u courant.
+	* @param const double& i - entier qui sera multiplié avec le vecteur u courant.
+	* @return const Vecteur2D - vecteur résultant de la multiplication de l'entier i et du vecteur u courant.
 	*/
-	const Vecteur2D operator*(const int& i) const;
+	const Vecteur2D operator*(const double& i) const;
 
 	/**
 	* Opérateur de cast. Equivalent d'une méthode toString().
