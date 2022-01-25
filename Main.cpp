671b500bbd3b9ce2f0e8cd1c7431f8b9c3882861
medkan01 <mehdiakniou@gmail.com>
2022-01-25 23:28:52 +0100

    ajout main (pour réaliser les tests pour l'instant)

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "TestVecteur2D.h"
+#include "TestRadian.h"
+#include "TestDegree.h"
+
+int main() {
+	TestVecteur2D::sessionTest();
+	TestRadian::sessionTest();
+	TestDegree::sessionTest();
+
+	return 0;
+}
