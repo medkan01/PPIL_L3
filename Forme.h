b77c274604c30861f49889c1724339b91a5ae6eb
medkan01 <mehdiakniou@gmail.com>
2022-01-26 09:22:00 +0100

    redefinition de Forme

--- Forme.h
+++ Forme.h
@@ -9,17 +9,17 @@ using namespace std;
 * Définie par une couleur et un point d'origine.
 */
 class Forme {
-	Couleur couleur;
-	Vecteur2D origine;
 public:
+	Couleur couleur;
+	Vecteur2D centre;
 	/**
 	* Constructeur de la classe Forme.
 	* 
 	* @param const string& couleur - Couleur de la forme.
 	* @param const Vectuer2D& origine - Origine de la forme.
 	*/
-	Forme(const Couleur& couleur, const Vecteur2D& origine) : 
-		couleur(couleur), origine(origine) {}
+	Forme(const Couleur& couleur, const Vecteur2D& centre) : 
+		couleur(couleur), centre(centre) {}
 
 	/**
 	* Effectue une translation sur la forme.
@@ -52,32 +52,18 @@ public:
 	virtual operator string() const = 0;
 
 	/**
-	* Récupère la couleur de la forme.
-	*
-	* @return string - couleur de la forme.
-	*/
-	const Couleur getCouleur() const { return couleur; }
-
-	/**
-	* Modifie la couleur de la forme.
-	*
-	* @param const string& c - nouvelle couleur de la forme.
-	*/
-	void setCouleur(const string& c) { couleur = c; }
-
-	/**
 	* Récupère l'origine de la forme.
 	* 
 	* @return const Vecteur2D - origine de la forme.
 	*/
-	const Vecteur2D getOrigine() const { return origine; }
+	const Vecteur2D getOrigine() const { return centre; }
 
 	/**
 	* Modifie l'origine de la forme.
 	* 
 	* @param const Vecteur2D& u - nouvelle origine de la forme.
 	*/
-	void setOrigine(const Vecteur2D& u) { origine = u; }
+	void setOrigine(const Vecteur2D& u) { centre = u; }
 
 	/**
 	* Clone une forme.
